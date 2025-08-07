a3a3fe10403dc6b0e4b461ad0da47704634e0c79
Okan <eroksuzrizaokan@gmail.com>
2025-08-07 15:16:21 -0500

    odd and even problem commited

---
+++ src/riza/CheckNumberOddOrEven.java
@@ -0,0 +1,17 @@
+package riza;
+
+public class CheckNumberOddOrEven {
+    public static void main(String[] args) {
+
+    }
+
+    public static void checkNumber(int num){
+
+        if (num%2 == 0){
+            System.out.println("Number is even!");
+        }
+        else {
+            System.out.println("Number is odd!");
+        }
+    }
+}
