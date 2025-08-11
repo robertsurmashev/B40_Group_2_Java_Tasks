8bd2aaf78a5a0bd6b4d2912a404d22fefa530131
DavidCydeo <david@cydeo.com>
2025-08-12 00:00:34 +0300

    Task 2 and 3 added

---
+++ src/WeeklyTasksFromDavid/Task3_swapTwoNumbers.java
@@ -0,0 +1,16 @@
+package WeeklyTasksFromDavid;
+
+public class Task3_swapTwoNumbers {
+    public static void main(String[] args) {
+
+
+        //Swap two variables' values without using a third variable
+
+
+
+
+
+    }
+
+
+}
