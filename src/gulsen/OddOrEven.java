fdc103d604ea8ed904cad728c01a2cb258bb68c7
gulsenyurdakul <gulsenyurdakul@hotmail.com>
2025-08-13 04:11:17 -0700

    system works

---
+++ src/gulsen/OddOrEven.java
@@ -0,0 +1,19 @@
+package gulsen;
+
+import javax.management.MBeanAttributeInfo;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        oddOrEven(5);
+        oddOrEven(44);
+    }
+
+    public static void oddOrEven(int num){
+
+        if (num%2==0){
+            System.out.println("number is EVEN = " + num);
+        }else {
+            System.out.println("number is ODD = " + num);
+        }
+    }
+}
