60bdb7b83a4f465c551d0211839cc33433d3daee
gulsenyurdakul <gulsenyurdakul@hotmail.com>
2025-08-13 05:13:03 -0700

    finra works

---
+++ src/gulsen/Finra.java
@@ -0,0 +1,23 @@
+package gulsen;
+
+public class Finra {
+    public static void main(String[] args) {
+        finra();
+    }
+
+    public static void finra() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            }else if (i % 3 == 0){
+                System.out.print("FIN ");
+            }else if (i % 5 == 0) {
+                System.out.print("RA ");
+            }else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    }
+
