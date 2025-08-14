14e8d2f9568dc634fb6cc0052fe34eb2c5932345
morchd <morchd11@gmail.com>
2025-08-14 13:04:55 -0600

    Task2 solution.

---
+++ src/HICHAM/Task3_swapTwoNumbers.java
@@ -0,0 +1,40 @@
+package HICHAM;
+
+public class Task3_swapTwoNumbers {
+//package WeeklyTasksFromDavid;
+
+
+        public static void main(String[] args) {
+
+
+            //Swap two variables' values without using a third variable
+
+
+
+
+
+                    int a = 5;
+                    int b = 10;
+
+                    System.out.println("Before swap:");
+                    System.out.println("a = " + a + ", b = " + b);
+
+                    // Swapping without a third variable
+                    a = a + b;  // a becomes 15
+                    b = a - b;  // b becomes 5
+                    a = a - b;  // a becomes 10
+
+                    System.out.println("After swap:");
+                    System.out.println("a = " + a + ", b = " + b);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
