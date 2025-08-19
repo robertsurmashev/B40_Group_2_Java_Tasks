9743c99cd566d2265f1dd61d598319b8afe871d0
DavidCydeo <david@cydeo.com>
2025-08-19 19:57:03 +0300

    Task 4 added-modified

--- src/WeeklyTasksFromDavid/Task4_reverseNegativeNumber.java
+++ src/WeeklyTasksFromDavid/Task4_reverseNegativeNumber.java
@@ -9,34 +9,9 @@ public class Task4_reverseNegativeNumber {
 
     public static void main(String[] args) {
 
-        System.out.println(reverseNum(-50));
+
     }
 
-    public static int reverseNumber(int number){
-        if(number < 10 && number >= 0){
-            return number * -1;
-        }
 
-        int result = 0;
-        while (number !=0) {
-            result = result * 10 + number % 10;
-            number /= 10;
-        }
-
-        return result;
-    }
-
-    //alternative solution
-    //converts&returns int num to a String so that we can handle -50 -> -5 issue because int does not have 0 before a number
-    public static String reverseNum(int num) {
-
-        String str = new StringBuilder("" + num).reverse().toString();
-
-        if (num < 0) {
-            str = "-" + str.substring(0, str.length() - 1);
-        }
-
-        return str;
-    }
 }
 
