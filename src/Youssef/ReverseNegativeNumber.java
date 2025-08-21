87ff9b2856fe00d2a2cf3c8538fe10fb8bc3b7b7
youssef <youssefel55.el@gmail.com>
2025-08-21 17:43:14 +0100

    Finished Task 3 of Reversed negative number

---
+++ src/Youssef/ReverseNegativeNumber.java
@@ -0,0 +1,24 @@
+package Youssef;
+
+public class ReverseNegativeNumber {
+
+   public static int reversedNegativeNumber(int num) {
+
+       String str = Integer.toString(num);
+
+       String positivePart = str.substring(1);
+
+       String reversed = new StringBuilder(positivePart).reverse().toString();
+
+       String finalStr = "-" + reversed;
+
+       return Integer.parseInt(finalStr);
+
+   }
+   public static void main(String[] args) {
+
+       System.out.println(reversedNegativeNumber(-98));
+
+   }
+
+}
