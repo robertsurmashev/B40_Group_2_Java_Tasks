8781b7f982448d532ed0eb1fe84745ca4de6a163
mohamedazzawi90 <Baghdad@80>
2025-08-20 21:28:33 -0700

    Method has been created and tested.

--- src/WeeklyTasksFromDavid/Task3_swapTwoNumbers.java
+++ src/WeeklyTasksFromDavid/Task3_swapTwoNumbers.java
@@ -3,14 +3,24 @@ package WeeklyTasksFromDavid;
 public class Task3_swapTwoNumbers {
     public static void main(String[] args) {
 
-
-        //Swap two variables' values without using a third variable
-
-
-
-
+        swapWithoutTemp(3, 2);
 
     }
 
 
+    //Swap two variables' values without using a third variable
+
+    public static void swapWithoutTemp(int a, int b) {
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        a = a + b; // sum of both
+        b = a - b; // subtract b from sum
+        a = a - b; // subtract new b from sum
+
+        System.out.println("After swap: a = " + a + ", b = " + b);
+
+
+    }
+
 }
+
