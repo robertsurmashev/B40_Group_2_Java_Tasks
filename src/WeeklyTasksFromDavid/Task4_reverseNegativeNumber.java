7d22f6f59eca1089a0e22f01f1c60cd5b50d2cec
DavidCydeo <david@cydeo.com>
2025-08-26 16:54:47 +0300

    Task 4 5 added

---
+++ src/WeeklyTasksFromDavid/Task4_reverseNegativeNumber.java
@@ -0,0 +1,17 @@
+package WeeklyTasksFromDavid;
+
+public class Task4_reverseNegativeNumber {
+
+
+    // reverseNegativeNumber
+    // Write a return method that can reverse digits of a negative number and return it as int
+    // input: -35  output: -53
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
+
